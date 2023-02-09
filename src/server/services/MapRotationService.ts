a877fc64cfc01256fc23586f80181b035376fc4a
r-unic <roleyspersonal@gmail.com>
2023-02-09 11:23:42 -0800

    Fix map rotation algorithm

--- src/server/services/MapRotationService.ts
+++ src/server/services/MapRotationService.ts
@@ -14,28 +14,8 @@ export class MapRotationService {
 
   public constructor() {
     const date = DateTime.now();
-    const { Day: day, Month: month } = date.ToUniversalTime();
-    const daysInMonth = this._getMonthDays(month);
-    const weeksInMonth = daysInMonth / 7;
+    const { Day: day } = date.ToUniversalTime();
     const week = math.ceil(day / 7);
-    const weekRounded = math.ceil(week / weeksInMonth);
-    print(week / weeksInMonth, weekRounded);
-    this.maps = rotations[weekRounded - 1];
-  }
-
-  private _getMonthDays(month: number): number {
-    switch (month) {
-      case 4:
-      case 6:
-      case 9:
-      case 11:
-        return 30;
-
-      case 2:
-        return 28;
-
-      default:
-        return 31;
-    }
+    this.maps = rotations[week - 1];
   }
 }
