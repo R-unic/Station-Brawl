5c4defc5a7fbef0d0c835c28a6176b9a75b347c8
r-unic <roleyspersonal@gmail.com>
2023-02-09 10:31:13 -0800

    Add MapRotationService

---
+++ src/server/services/MapRotationService.ts
@@ -0,0 +1,40 @@
+import { Service } from "@flamework/core";
+
+const rotations = [
+  ["Gas Station", "Gas Station", "Gas Station", "Gas Station", "Gas Station"],
+  ["Gas Station", "Gas Station", "Gas Station", "Gas Station", "Gas Station"],
+  ["Gas Station", "Gas Station", "Gas Station", "Gas Station", "Gas Station"],
+  ["Gas Station", "Gas Station", "Gas Station", "Gas Station", "Gas Station"],
+  ["Gas Station", "Gas Station", "Gas Station", "Gas Station", "Gas Station"]
+];
+
+@Service({})
+export class MapRotationService {
+  public readonly maps: string[]
+
+  public constructor() {
+    const date = DateTime.now();
+    const { Day: day, Month: month } = date.ToUniversalTime();
+    const daysInMonth = this._getMonthDays(month);
+    const weeksInMonth = daysInMonth / 7;
+    const week = math.ceil(day / 7);
+    const weekRounded = math.ceil(week / weeksInMonth);
+    this.maps = rotations[weekRounded - 1];
+  }
+
+  private _getMonthDays(month: number): number {
+    switch (month) {
+      case 4:
+      case 6:
+      case 9:
+      case 11:
+        return 30;
+
+      case 2:
+        return 28;
+
+      default:
+        return 31;
+    }
+  }
+}
